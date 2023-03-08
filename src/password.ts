367f17f847aed77601ae87a4c34a1a4adebe846d
MykytaSikachyna <sikachyna.n@gmail.com>
2023-03-08 18:06:07 +0200

    add generateCharset function & edit generate password function

--- src/password.ts
+++ src/password.ts
@@ -6,35 +6,44 @@ import { randomBytes, pbkdf2Sync } from 'crypto'
  * @property {string} salt - The salt used for hashing
  * */
 
+const generateCharset = (
+  useUpper: boolean, 
+  useNumbers: boolean,
+  useSpecial: boolean
+):string => {
+  let charset = 'abcdefghijklmnoprstuvwxyz'
+  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const numbers = '0123456789'
+  const special = '~`!@#$%^&*()_-+={[}]|:;\"\'<,>.?/'
+
+  if (useUpper) { charset += upper }
+  if (useNumbers) { charset += numbers }
+  if (useSpecial) { charset += special }
+
+  return charset
+}
+
 /**
  * @param {string} length
+ * @param {boolean} useUpper
+ * @param {boolean} useNumber
  * @param {boolean} useSpecial
  * @returns {string} password
  * */
 export const generatePassword = (
   length: number,
+  useUpper: boolean,
+  useNumber: boolean,
   useSpecial: boolean
 ): string => {
-  const leters = 'abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  const numbers = '0123456789'
-  const special = "~`!@#$%^&*()_-+={[}]|:;'<,>.?/"
+  const charset = generateCharset(useUpper, useNumber, useSpecial)
   let pswd = ''
 
-  if (useSpecial) {
-    let charset = leters + numbers + special
-    for (let i = 0; i < length; i++) {
-      pswd += charset.charAt(Math.floor(Math.random() * charset.length))
-    }
-
-    return pswd
-  } else {
-    let charset = leters + numbers
-    for (let i = 0; i < length; i++) {
-      pswd += charset.charAt(Math.floor(Math.random() * charset.length))
-    }
-
-    return pswd
+  for (let i = 0; i < length; i++) {
+    pswd += charset.charAt(Math.floor(Math.random() * charset.length))
   }
+
+  return pswd
 }
 
 /**
