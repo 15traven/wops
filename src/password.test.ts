567d033b8adb1f8d4074eb757d02813ca5623f94
MykytaSikachyna <sikachyna.n@gmail.com>
2023-03-08 21:39:10 +0200

    add tests for checkPasswordStrength function

--- src/password.test.ts
+++ src/password.test.ts
@@ -1,6 +1,11 @@
-import { hashPassword, validatePassword } from './password'
+import { checkPasswordStrength, hashPassword, validatePassword } from './password'
 
 const PASSWORD = '12345'
+const VERY_WEAK_PASSWORD = '1234567'
+const WEAK_PASSWORD = '1qwerty'
+const MODERATE_PASSWORD = '12345qwerty'
+const STRONG_PASSWORD = '12345Qwerty'
+const VERY_STRONG_PASSWORD = '!12345Qwerty'
 const VALUES = {
   hash: '2c6dec7c357784de002795c9b771f5e50d1219fc2c3f1ad96f732f1ae532a666a49b3a732d6a3cf20ff734b21784b5c174c808ce5ce18a08a0179deec792d883',
   salt: 'a25354844920ff905464891c10a7490a',
@@ -29,5 +34,32 @@ describe('validatePassword', () => {
     expect(validatePassword(VALUES.wrongString, VALUES.salt, VALUES.hash)).toBe(
       false
     )
+  })
+})
+
+describe('checkPasswordStrength', () => {
+  test('with very weak password', () => {
+    const result = checkPasswordStrength(VERY_WEAK_PASSWORD)
+    expect(result).toBe('very weak')
+  })
+
+  test('with weak password', () => {
+    const result = checkPasswordStrength(WEAK_PASSWORD)
+    expect(result).toBe('weak')
+  })
+
+  test('with modered password', () => {
+    const result = checkPasswordStrength(MODERATE_PASSWORD)
+    expect(result).toBe('moderate')
+  }) 
+
+  test('with strong password', () => {
+    const result = checkPasswordStrength(STRONG_PASSWORD)
+    expect(result).toBe('strong')
+  }) 
+
+  test('with very strong password', () => {
+    const result = checkPasswordStrength(VERY_STRONG_PASSWORD)
+    expect(result).toBe('very strong')
   })
 })
